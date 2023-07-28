ee001f774f3260790850859216ecb701b82fddd4
raid <raid@debian.local>
2023-07-28 22:37:33 +0200

    reenabled the build of mod-host and plugins after testing

--- run_build.sh
+++ run_build.sh
@@ -10,8 +10,8 @@ mkdir -p /home/$USER_NAME/mod-user-files
 mkdir -p /home/$USER_NAME/mod-data
 mkdir -p /home/$USER_NAME/mod-pedalboards
 
-#docker build  --rm -f "mod-host/Dockerfile" --build-arg PUID=${PUID} --build-arg PGID=${PGID}  -t mod-host "mod-host" 
-#docker build  --rm -f "mod-plugin-builder/Dockerfile" --build-arg PUID=${PUID} --build-arg PGID=${PGID} -t mod-plugin-builder "mod-plugin-builder" 
+docker build  --rm -f "mod-host/Dockerfile" --build-arg PUID=${PUID} --build-arg PGID=${PGID}  -t mod-host "mod-host" 
+docker build  --rm -f "mod-plugin-builder/Dockerfile" --build-arg PUID=${PUID} --build-arg PGID=${PGID} -t mod-plugin-builder "mod-plugin-builder" 
 docker build  --rm -f "mod-ui/Dockerfile" -t mod-ui "mod-ui"
 
 docker-compose up  --no-build mod-ui
